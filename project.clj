c6d451f366f12135751971d6ce9d9b1499226e44
semen1032 <51064226+semen1032@users.noreply.github.com>
2019-12-14 11:08:24 +0300

    выполнена 3 лабораторная работа

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject metrics-server "1.0.0"
+  :description "Sample server for stunents labwork"
+  :license {:name "Apache 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
+  :main metrics-server.core
+  ; :main metrics-server.api.hardware
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clj-http "3.6.0"]
+                 [cheshire "5.5.0"]])
+
